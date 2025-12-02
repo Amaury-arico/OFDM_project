77dac3587a69719f1e3bf8eafd5b22f3376c50fb
Amaury-arico <arico.amaury@gmail.com>
2025-12-02 16:24:14 +0100

    Channel_Estimation_SRC_step_2

--- src/equalize_chan.m
+++ src/equalize_chan.m
@@ -1,6 +1,14 @@
-function [signal_end] = equalize_chan(signal,h,cfg)
-    
-   H=fft(h,cfg.N_sub).';
-   signal_end = signal./H;
+function [signal_end] = equalize_chan(signal,h,mode,cfg)
+
+   if strcmp(mode, 'time_est')
+       if size(h,1) == 1
+       else
+           h = h.';
+       end
+       H=fft(h,cfg.N_sub).';
+       signal_end = signal./H;
+   else
+       signal_end = signal./h;
+   end
     
 end
